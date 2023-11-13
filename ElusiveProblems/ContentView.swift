0daf6097b43d471fa03a6f65447c452d875a2a44
kitcurio <kasiarivers@gmail.com>
2023-11-13 11:24:14 -0500

    Initial Commit

---
+++ ElusiveProblems/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElusiveProblems
+//
+//  Created by Kasia Rivers on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElusiveProblems/ElusiveProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElusiveProblemsApp.swift
+//  ElusiveProblems
+//
+//  Created by Kasia Rivers on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ElusiveProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
